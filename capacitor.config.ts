eddc0d89264ffdf88fd772b62113e2ac7555bd6b
sarvosmi <vaishali.nimbolkar@hotmail.com>
2023-05-26 17:29:58 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'reactiveFormDemo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
